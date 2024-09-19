8baaae27759aecebff67cf785ba03b902782bd79
Pauli <pauli.alakylmanen@yahoo.com>
2024-09-19 11:00:15 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Green from './components/Green';
+import Blue from './components/Blue';
+import Yellow from './components/Yellow';
+function App () {
+
+    return (
+    <div className="App">
+      <Header />
+      <Green />
+      <Blue />
+      <Yellow />
+    </div>
+  );
+}
+
+
+export default App;
